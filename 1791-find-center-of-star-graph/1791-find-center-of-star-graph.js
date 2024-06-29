917beb568868a8fdd8d29294080ed1905bcde6f7
Shivrajsinh Zala <76481395+shivrajsinhzala@users.noreply.github.com>
2024-06-30 00:12:38 +0530

    Time: 81 ms (74.94%), Space: 63.9 MB (91.86%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function(edges) {
+  if(edges[0][0] == edges[1][0]) return edges[0][0]
+  else if(edges[0][1] == edges[1][0]) return edges[1][0]
+  else{
+    return edges[1][1]
+    }
+};
